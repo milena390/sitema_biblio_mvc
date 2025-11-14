3bde8b237e74cbd2d099fcab17786302ed04c1dd
milena390 <milena.silva390@etec.sp.gov.br>
2025-11-14 11:14:41 -0300

    ok

--- php_poo_mvc_3-main/php_poo_mvc_3-main/project-app/app/Views/layouts/main.php
+++ php_poo_mvc_3-main/php_poo_mvc_3-main/project-app/app/Views/layouts/main.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?? 'Biblioteca PM'; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://localhost/sitema_biblio_mvc/php_poo_mvc_3-main/php_poo_mvc_3-main/project-app/app/Views/layouts/style.css" rel="stylesheet">
 </head>
 <body>
     <?php
